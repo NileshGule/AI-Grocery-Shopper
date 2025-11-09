9d0ea235bcd0b571f1dbde8e719243ee855dd9eb
Nilesh Gule <vn_nilesh@yahoo.com>
2025-11-09 12:05:51 +1100

    removed commented code

--- src/Agents/TestAgent/TestAgentApp/Program.cs
+++ src/Agents/TestAgent/TestAgentApp/Program.cs
@@ -1,36 +1,4 @@
-﻿// using OpenAI.Chat;
-// using Azure;
-// using Azure.AI.OpenAI;
-
-// var endpoint = new Uri("https://ai-foundry-ai-hub.cognitiveservices.azure.com/");
-// var deploymentName = "gpt-4.1-mini";
-// var apiKey = "ECPNp5Z1PN7rh9cK4y28mxVNovrXQrFN0SdmOQoPXdqhGZUBEHmEJQQJ99BHACYeBjFXJ3w3AAAAACOG5iIT";
-
-// AzureOpenAIClient azureClient = new(
-//     endpoint,
-//     new AzureKeyCredential(apiKey));
-// ChatClient chatClient = azureClient.GetChatClient(deploymentName);
-
-// // var requestOptions = new ChatCompletionOptions()
-// // {
-// //     MaxCompletionTokens = 13107,
-// //     Temperature = 1.0f,
-// //     TopP = 1.0f,
-// //     FrequencyPenalty = 0.0f,
-// //     PresencePenalty = 0.0f,
-
-// // };
-
-// List<ChatMessage> messages = new List<ChatMessage>()
-// {
-//     new SystemChatMessage("You are a helpful assistant."),
-//     new UserChatMessage("I am going to Paris, what should I see?"),
-// };
-
-// var response = chatClient.CompleteChat(messages);
-// System.Console.WriteLine(response.Value.Content[0].Text);
-
-using System;
+﻿using System;
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using Azure.AI.OpenAI;
